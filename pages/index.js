43f21f0ee5b45d7348c53c5c4565ebd426683c9d
micahweiss <m.d.weiss25@gmail.com>
2023-10-28 10:40:49 -0400

    init

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+
+import Loader from '../components/Loader';
+
+export default function Home() {
+    return (
+        <div>
+            fuck
+            <Loader show />
+        </div>
+    );
+}
